0d8101b691e1853ec978fc9ae55804c6dc06602a
Du Yizhuo <dyzdyz010@gmail.com>
2023-03-22 21:23:08 +0800

    Update content and style

--- org-settings.el
+++ org-settings.el
@@ -24,6 +24,18 @@
 
 ;;; Code:
 
+(require 'ox-latex)
+(unless (boundp 'org-latex-classes)
+  (setq org-latex-classes nil))
+(add-to-list 'org-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
 (setq org-latex-compiler "lualatex"
       org-latex-default-packages-alist nil
       org-latex-default-table-environment "longtable"
